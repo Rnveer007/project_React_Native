77675e7b90e4db62de19e34ff6cef88acf75cd0f
Rnveer007 <rvs523474@gmail.com>
2025-03-28 15:07:00 +0530

    updated

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={style.container}>
+      <Text style={style.text}>Home Screen</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 20,
+  }
+})
